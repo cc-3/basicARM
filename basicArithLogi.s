7945167e3bceb3a428c05ca22cba865d90d316b2
Ali Lemus <alilemus@galileo.edu>
2017-02-08 00:52:31 +0000

    add basicArithLogi whitch is a small program testing various simple ALU operations

---
+++ basicArithLogi.s
@@ -0,0 +1,14 @@
+	.text
+	.globl main
+
+// just testing some adds, subs and logic operations
+
+main:
+	mov x1, #1
+	mov x2, #5
+	add x0, x1, #3
+	sub x0, x0, #2
+//	orr x0, x0, #5 wtf?
+	orr x0, x0, x2
+	ret
+
